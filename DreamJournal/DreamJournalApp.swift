1ec3c1e6cce35c1eb116138170c7acb166d02a45
tanya_bond <bondarenkotatiana96@gmail.com>
2022-07-06 09:43:13 -0700

    Initial commit

---
+++ DreamJournal/DreamJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamJournalApp.swift
+//  DreamJournal
+//
+//  Created by Tatiana Bondarenko on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct DreamJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
